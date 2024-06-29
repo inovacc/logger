76bdd9e26ee9672697cab01054b71d93b78c10a0
Dyam Marcano <dyam.marcano@gmail.com>
2024-06-28 21:14:44 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/inovacc/logger
+
+go 1.22
+
+require (
+	github.com/inovacc/cron v0.0.0-20240628230802-1735fdd38752
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
+)
---
+++ logger.go
@@ -0,0 +1,104 @@
+package logger
+
+import (
+	"context"
+	"github.com/inovacc/cron"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"io"
+	"log/slog"
+	"os"
+	"strings"
+)
+
+const (
+	logExtension        = ".log"
+	loggerSchedulerName = "log-rotation"
+)
+
+var (
+	d         *logRotator
+	scheduler *cron.Cron
+)
+
+func init() {
+	d = &logRotator{tee: os.Stdout}
+	slog.SetDefault(slog.New(slog.NewJSONHandler(d.tee, nil)))
+}
+
+type logRotator struct {
+	filename     string
+	maxSize      int
+	maxAge       int
+	maxBackups   int
+	localTime    bool
+	compress     bool
+	rotationTime string
+	stdout       bool
+	tee          io.Writer
+}
+
+type RotatedLogger struct {
+	*lumberjack.Logger
+}
+
+// NewRotatorHandler creates a new RotatedLogger that implements lumberjack.Logger compatible with slog.Handler
+func NewRotatorHandler(filename string, maxSize int, maxAge int, maxBackups int, localTime bool, compress bool) *RotatedLogger {
+	if !strings.Contains(filename, logExtension) {
+		filename += logExtension
+	}
+
+	return &RotatedLogger{
+		Logger: &lumberjack.Logger{
+			Filename:   filename,
+			MaxSize:    maxSize,
+			MaxBackups: maxBackups,
+			MaxAge:     maxAge,
+			Compress:   compress,
+			LocalTime:  localTime,
+		},
+	}
+}
+
+// SetScheduler sets up a cron job to rotate logs
+func (r *RotatedLogger) SetScheduler(spec string) {
+	newCronScheduler, err := cron.NewCronScheduler(context.Background())
+	if err != nil {
+		slog.Error("error setting up cron scheduler", slog.String(loggerSchedulerName, err.Error()))
+	}
+
+	scheduler = newCronScheduler
+
+	if _, err = scheduler.AddFunc(spec, func() {
+		if err = r.Logger.Rotate(); err != nil {
+			slog.Error("error rotating logs", slog.String(loggerSchedulerName, err.Error()))
+		}
+	}); err != nil {
+		slog.Error("error setting up cron job", slog.String(loggerSchedulerName, err.Error()))
+	}
+}
+
+// Rotate rotates the logs manually
+func (r *RotatedLogger) Rotate() error {
+	return r.Logger.Rotate()
+}
+
+// NewLoggerWithJSONRotator creates a new slog.Logger with a RotatedLogger handler and JSON formatter
+func NewLoggerWithJSONRotator(rotator *RotatedLogger, opts *slog.HandlerOptions) {
+	if !d.stdout {
+		d.tee = io.MultiWriter(rotator, d.tee)
+	}
+	slog.SetDefault(slog.New(slog.NewJSONHandler(d.tee, opts)))
+}
+
+// NewLoggerWithTextRotator creates a new slog.Logger with a RotatedLogger handler and Text formatter
+func NewLoggerWithTextRotator(rotator *RotatedLogger, opts *slog.HandlerOptions) {
+	if !d.stdout {
+		d.tee = io.MultiWriter(rotator, d.tee)
+	}
+	slog.SetDefault(slog.New(slog.NewTextHandler(d.tee, opts)))
+}
+
+// EnableStdout enables logging to stdout and ignores writing to file
+func EnableStdout() {
+	d.stdout = true
+}
